5f8ebaea0ae67c5cf0f8d506db284607e7ceadc8
alexnesov <alexandrenesovic@gmail.com>
2022-04-22 23:31:28 +0200

    ECommerce init

---
+++ frontend/src/App.js
@@ -0,0 +1,12 @@
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <p>ECommerce</p>
+      </header>
+    </div>
+  );
+}
+
+export default App;
