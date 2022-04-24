0a71067c27d7c47aaa2a16d8ac06b0c20b6a7e16
alexnesov <alexandrenesovic@gmail.com>
2022-04-24 19:33:58 +0200

    resolved react routing pb

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,23 +1,26 @@
+import { Container } from 'react-bootstrap'
+import { HashRouter as Router, Route, Routes } from 'react-router-dom'
+
 import Header from './components/Header'
 import Footer from './components/Footer'
-import { Container } from 'react-bootstrap'
-
-
-import HomeScreen from './screens/HomeScreen';
+import HomeScreen from './screens/HomeScreen'
+import ProductScreen from './screens/ProductScreen'
 
 function App() {
   return (
-    <div className="App">
-      <Header />
-      <main className='py-5'>
-      <Container>
-        <h1>Welcome</h1>
-          <HomeScreen />
-      </Container>
+  <Router>
+    <Header />
+      <main className='py-3'>
+          <Container>
+            <Routes>
+              <Route path='/' exact element={ <HomeScreen/> }  />
+              <Route path='/product/:id' element={<ProductScreen/>} />
+            </Routes>
+          </Container>
       </main>
       <Footer />
-    </div>
-  );
+  </Router>
+);
 }
 
 export default App;
