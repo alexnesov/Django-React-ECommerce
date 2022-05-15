bfced4495ff0afd11bdc8fa2a482d8497fe41bf4
alexnesov <alexandrenesovic@gmail.com>
2022-05-15 11:53:39 +0200

    img upload in static in backend feature impl

--- backend/settings.py
+++ backend/settings.py
@@ -122,7 +122,13 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL          = 'backend/static/'
+STATICFILES_DIRS    = [
+    BASE_DIR / 'backend/static'
+]
+
+
+MEDIA_ROOT = 'backend/static/images'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
