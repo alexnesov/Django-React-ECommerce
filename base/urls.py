b08015620af2531fd98defcba79c2447f001b473
alexnesov <alexandrenesovic@gmail.com>
2022-08-17 23:05:35 +0200

    user serializer

--- base/urls.py
+++ base/urls.py
@@ -2,8 +2,18 @@ from django.urls import path
 from . import views
 
 
+
+
 urlpatterns = [
     path('', views.getRoutes, name="routes"),
+    
     path('api/products/', views.getProducts, name="products"),
     path('api/products/<str:pk>', views.getProduct, name="products"),
+    
+    path('api/users/login', views.MyTokenObtainPairView.as_view(), 
+          name='token_obtain_pair'),
+
+
+    path('api/users/profile', views.getUserProfile, name="users-profile"),
+
 ]
--- base/views.py
+++ base/views.py
@@ -1,4 +1,5 @@
 import imp
+from turtle import update
 from django.shortcuts import render
 from django.http import JsonResponse
 from.products import products
@@ -9,7 +10,28 @@ from rest_framework.response import Response
 
 from .models import Product
 from .products import products
-from .serializers import ProductSerializer
+from .serializers import ProductSerializer, UserSerializer
+
+
+from rest_framework_simplejwt.serializers import TokenObtainPairSerializer
+from rest_framework_simplejwt.views import TokenObtainPairView
+
+
+
+class MyTokenObtainPairSerializer(TokenObtainPairSerializer):
+
+    def validate(self, attrs):
+        data = super().validate(attrs)
+
+        data['username']    = self.user.username
+        data['email']       = self.user.email
+
+
+        return data
+
+
+class MyTokenObtainPairView(TokenObtainPairView):
+    serializer_class = MyTokenObtainPairSerializer
 
 
 @api_view(['GET'])
@@ -42,9 +64,20 @@ def getProducts(request):
 def getProduct(request, pk):
     """
     """
-    
-    print("\n\n TEST pk: ", pk)
     product         = Product.objects.get(_id = pk)
-    serializer      = ProductSerializer(product, many= False)
+    serializer      = ProductSerializer(product, many=False)
 
     return Response(serializer.data)
+
+
+
+
+@api_view(['GET'])
+def getUserProfile(request):
+    """
+    """
+    user            = request.user
+    serializer      = UserSerializer(user, many=False)
+
+    return Response(serializer.data)
+
