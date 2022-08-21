3714bf8a594ec497740f8d761fd3330598132ebc
alexnesov <alexandrenesovic@gmail.com>
2022-08-21 15:36:41 +0200

    register user error handler impl

--- base/urls.py
+++ base/urls.py
@@ -5,15 +5,18 @@ from . import views
 
 
 urlpatterns = [
-    path('', views.getRoutes, name="routes"),
     
-    path('api/products/', views.getProducts, name="products"),
-    path('api/products/<str:pk>', views.getProduct, name="products"),
-    
+    path('', views.getProducts, name='token_obtain_pair'),
+
     path('api/users/login', views.MyTokenObtainPairView.as_view(), 
           name='token_obtain_pair'),
+
+    path('api/users/register/', views.registerUser, name='register'),
 
     path('api/users/profile', views.getUserProfile, name="users-profile"),
     path('api/users', views.getUsers, name="users-profile"),
 
+    path('api/products/', views.getProducts, name="products"),
+    path('api/products/<str:pk>', views.getProduct, name="products"),
+    
 ]
--- base/views.py
+++ base/views.py
@@ -1,3 +1,4 @@
+import email
 import imp
 from turtle import update
 from django.shortcuts import render
@@ -12,12 +13,13 @@ from .models import Product
 from .products import products
 from .serializers import ProductSerializer, UserSerializer, UserSerializerWithToken
 
-
 from rest_framework_simplejwt.serializers import TokenObtainPairSerializer
 from rest_framework_simplejwt.views import TokenObtainPairView
 
-
 from django.contrib.auth.models import User
+
+from django.contrib.auth.hashers import make_password
+from rest_framework import status
 
 
 class MyTokenObtainPairSerializer(TokenObtainPairSerializer):
@@ -37,21 +39,28 @@ class MyTokenObtainPairView(TokenObtainPairView):
     serializer_class = MyTokenObtainPairSerializer
 
 
-@api_view(['GET'])
-def getRoutes(request):
+@api_view(['POST'])
+def registerUser(request):
 
-    routes = [
-        '/api/products/',
-        '/api/products/create/',
-        '/api/products/upload/',
-        '/api/products/<id>/reviews/',
-        '/api/products/top/',
-        '/api/products/<id>/',
-        '/api/products/delete/<id>',
-        '/api/products/<update>/<id>',
-    ]
-    
-    return Response(routes)
+    data = request.data
+
+    try:
+        print('Data: ', data)
+
+        user = User.objects.create(
+            first_name      = data['name'],
+            username        = data['email'],
+            email           = data['email'],
+            password        = make_password(data['password'])
+        )
+
+        serializer = UserSerializerWithToken(user, many = False)
+
+        return Response(serializer.data) 
+    except:
+        message = {'detail':'User with this email already exists'}
+
+        return Response(message, status=status.HTTP_400_BAD_REQUEST)
 
 
 @api_view(['GET'])
