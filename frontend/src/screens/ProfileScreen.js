47170d807949a777fe51b48c23343ee4d9647652
alexnesov <alexandrenesovic@gmail.com>
2022-10-11 22:18:37 +0200

    profile screen

--- frontend/src/screens/ProfileScreen.js
+++ frontend/src/screens/ProfileScreen.js
@@ -1,18 +1,61 @@
 import React, { useState, useEffect } from 'react';
-import { Link } from 'react-router-dom';
 
 import { Form, Button, Row, Col } from 'react-bootstrap';
 import { useDispatch, useSelector } from 'react-redux';
 
-import Loader from '../components/Loader';
-import Message from '../components/Message';
-import FormContainer from '../components/FormContainer';
 import { register } from '../actions/userActions';
 
-import { getUserDetails } from '../actions/userActions'
+import { useParams, useNavigate } from "react-router-dom"
 
 
 function ProfileScreen( {history} ) {
+
+    const navigate                               = useNavigate()
+
+    const [name, setName]                        = useState('')
+    const [email, setEmail]                      = useState('')
+    const [password, setPassword]                = useState('')
+    const [confirmPassword, setConfirmPassword]  = useState('')
+    const [message, setMessage]                  = useState('')
+
+    const dispatch                               = useDispatch();
+    const test_param                             = useParams();
+
+    const redirect                               = test_param.search ? test_param.search.split('=')[1] : '/'
+    
+    const userDetails                            = useSelector(state => state.userDetails)
+    const { error, loading, user }               = userDetails
+    
+    const userLogin                              = useSelector(state => state.userLogin)
+    console.log("userLogin: ", userLogin)
+    const { userInfo }                           = userLogin
+    
+
+    
+    console.log("userInfo: ", userInfo)
+
+    useEffect(() => {
+        console.log("userLogin.userInfo: ", userLogin.userInfo)
+        if(!userLogin.userInfo){
+            console.log("====> Entered in useEffect !!!!!")
+            navigate('/login')
+        }
+    }, [userLogin.userInfo, redirect])
+
+
+    const submitHandler = (e) => {
+        e.preventDefault()
+        console.log('Submitted!')   
+
+        if(password != confirmPassword){
+            setMessage('Passwords do not match')
+        }else{
+            dispatch(register(name, email, password))
+        }
+
+    }
+
+
   return (
         <Row>
             <Col md={3}>
